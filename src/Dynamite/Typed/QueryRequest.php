e7e820784cc0e9730d8b14506aaea4b188bc0f06
pizzaminded <pizzaminded@users.noreply.github.com>
2020-12-25 20:05:53 +0100

    feat: QueryRequest toArray() and some with* methods

--- src/Dynamite/Typed/QueryRequest.php
+++ src/Dynamite/Typed/QueryRequest.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Dynamite\Typed;
 
+use Aws\DynamoDb\Marshaler;
+
 /**
  * @author pizzaminded <mikolajczajkowsky@gmail.com>
  * @see https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Query.html
@@ -14,22 +16,46 @@ class QueryRequest
     protected ?string $indexName = null;
     protected ?array $exclusiveStartKey = null;
     protected ?int $limit = null;
+    protected ?string $keyConditionExpression = null;
+    protected array $expressionAttributeValues = [];
+    protected array $expressionAttributeNames = [];
+    protected Marshaler $marshaler;
+
+    public function __construct(?Marshaler $marshaler = null)
+    {
+        $this->marshaler = $marshaler ?? new Marshaler();
+    }
 
     public function toArray(): array
     {
         $output = [];
         $output['TableName'] = $this->tableName;
 
-        if($this->limit !== null) {
+        if ($this->limit !== null) {
             $output['Limit'] = $this->limit;
         }
 
-        if($this->indexName !== null) {
+        if ($this->indexName !== null) {
             $output['IndexName'] = $this->indexName;
         }
 
-        if($this->exclusiveStartKey !== null) {
+        if ($this->exclusiveStartKey !== null) {
             $output['ExclusiveStartKey'] = $this->exclusiveStartKey;
+        }
+
+        if ($this->keyConditionExpression !== null) {
+            $output['KeyConditionExpression'] = $this->keyConditionExpression;
+        }
+
+        if (count($this->expressionAttributeNames) > 0) {
+            $output['ExpressionAttributeNames'] = $this->expressionAttributeNames;
+        }
+
+        if (count($this->expressionAttributeValues) > 0) {
+            $output['ExpressionAttributeValues'] =
+                $this->marshaler->marshalItem(
+                    $this->expressionAttributeValues
+                );
         }
 
         return $output;
@@ -54,5 +80,27 @@ class QueryRequest
         return $this;
     }
 
+    public function withLimit(int $limit)
+    {
+        $this->limit = $limit;
+        return $this;
+    }
 
+    public function withKeyConditionExpression(string $expr)
+    {
+        $this->keyConditionExpression = $expr;
+        return $this;
+    }
+
+    public function withExpressionAttributeName($placeholder, $val)
+    {
+        $this->expressionAttributeNames[$placeholder] = $val;
+        return $this;
+    }
+
+    public function withExpressionAttributeValue($placeholder, $val)
+    {
+        $this->expressionAttributeValues[$placeholder] = $val;
+        return $this;
+    }
 }
