dcbd969b692903d96229742e923e26c037aad37f
pizzaminded <pizzaminded@users.noreply.github.com>
2020-12-25 20:06:11 +0100

    feat: add phpdoc with return hint

--- src/Dynamite/Repository/AccessPatternsProviderInterface.php
+++ src/Dynamite/Repository/AccessPatternsProviderInterface.php
@@ -3,8 +3,12 @@ declare(strict_types=1);
 
 namespace Dynamite\Repository;
 
+use Dynamite\AccessPattern;
 
 interface AccessPatternsProviderInterface
 {
+    /**
+     * @return AccessPattern[]
+     */
     public function registerAccessPatterns(): array;
 }
