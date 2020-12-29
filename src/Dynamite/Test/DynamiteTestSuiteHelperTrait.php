1fffceee0d5123acda0f05d9ea70c6007f2bf6bd
pizzaminded <pizzaminded@users.noreply.github.com>
2020-12-29 04:12:46 +0100

    feat: add more methods to DynamiteTestSuiteHelperTrait
    Signed-off-by: pizzaminded <pizzaminded@users.noreply.github.com>

--- src/Dynamite/Test/DynamiteTestSuiteHelperTrait.php
+++ src/Dynamite/Test/DynamiteTestSuiteHelperTrait.php
@@ -5,13 +5,32 @@ namespace Dynamite\Test;
 
 
 use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\Reader;
+use Dynamite\ItemSerializer;
 use Dynamite\Mapping\ItemMappingReader;
 
 trait  DynamiteTestSuiteHelperTrait
 {
 
+    private ?Reader $_reader = null;
+
     private function createItemMappingReader(): ItemMappingReader
     {
-        return new ItemMappingReader(new AnnotationReader());
+        return new ItemMappingReader($this->getAnnotationReader());
+    }
+
+
+    private function getAnnotationReader(): Reader
+    {
+        if ($this->_reader === null) {
+            $this->_reader = new AnnotationReader();
+        }
+
+        return $this->_reader;
+    }
+
+    private function createItemSerializer(): ItemSerializer
+    {
+        return new ItemSerializer($this->getAnnotationReader());
     }
 }
