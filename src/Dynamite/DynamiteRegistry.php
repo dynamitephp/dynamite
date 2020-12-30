bbcc083bb4b4b475dede7d8d6e6643ab394a1d15
pizzaminded <pizzaminded@users.noreply.github.com>
2020-12-31 00:48:03 +0100

    feat: add getItemRepositoryFor() method
    Signed-off-by: pizzaminded <pizzaminded@users.noreply.github.com>

--- src/Dynamite/DynamiteRegistry.php
+++ src/Dynamite/DynamiteRegistry.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 namespace Dynamite;
 
 
+use Dynamite\Exception\DynamiteException;
+
 class DynamiteRegistry
 {
 
     /**
-     * @var array<string,Dynamite>
+     * @var Dynamite[]
+     * @psalm-var array<string,Dynamite>
      */
     protected array $managedTables = [];
 
@@ -19,5 +22,16 @@ class DynamiteRegistry
     public function addManagedTable(string $name, Dynamite $instance)
     {
         $this->managedTables[$name] = $instance;
+    }
+
+    public function getItemRepositoryFor(string $fqcn): ItemRepository
+    {
+        foreach ($this->managedTables as $instance) {
+            if ($instance->manages($fqcn)) {
+                return $instance->getItemRepository($fqcn);
+            }
+        }
+
+        throw new DynamiteException(sprintf('Could not find any instance which manage "%s" object.', $fqcn));
     }
 }
