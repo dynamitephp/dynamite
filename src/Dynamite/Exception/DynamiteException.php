4fa36af0ebbe26315cd944d91196b1f14f119d30
pizzaminded <pizzaminded@users.noreply.github.com>
2022-03-26 02:57:46 +0100

    refactor: use Exception instead of Error

--- src/Dynamite/Exception/DynamiteException.php
+++ src/Dynamite/Exception/DynamiteException.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Dynamite\Exception;
 
+use Exception;
+
 /**
+ * Any purpose-specific Exception must inherit this one.
  * @author pizzaminded <mikolajczajkowsky@gmail.com>
  * @license MIT
  */
-class DynamiteException extends \Error
+class DynamiteException extends Exception
 {
 }
