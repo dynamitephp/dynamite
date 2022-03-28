6b226ce398b0f323e207cc0913f8628b5a526c6d
pizzaminded <pizzaminded@users.noreply.github.com>
2022-03-29 01:13:26 +0200

    feat: add format filtering implementation

--- src/Dynamite/PrimaryKey/KeyFormatResolver.php
+++ src/Dynamite/PrimaryKey/KeyFormatResolver.php
@@ -4,14 +4,59 @@ declare(strict_types=1);
 
 namespace Dynamite\PrimaryKey;
 
+use Dynamite\Exception\FormatException;
 use Dynamite\Mapping\ItemMapping;
 
 class KeyFormatResolver
 {
+    protected const PLACEHOLDER_REGEXP = '/{(.*?)\}/';
+
+    /**
+     * @var array<string, ValueFilterInterface>
+     */
+    protected array $filters = [];
 
     public function resolve(string $format, ItemMapping $mapping, array $values): string
     {
+        preg_match_all(self::PLACEHOLDER_REGEXP, $format, $matches);
 
+        $placeholders = $matches[1] ?? [];
+
+        foreach ($placeholders as $key => $placeholder) {
+            $filters = [];
+            $propName = $placeholder;
+            if (str_contains($placeholder, ':')) {
+                $splitPlaceholder = explode(':', $placeholder);
+                $propName = array_pop($splitPlaceholder);
+                $filters = $splitPlaceholder;
+            }
+
+            $attribute = $mapping->getProperty($propName);
+            $attributeName = $attribute->getName();
+            $valueToInterpolate = $values[$attributeName];
+
+            foreach ($filters as $filter) {
+                if(!isset($this->filters[$filter])) {
+                    throw FormatException::filterNotFound($filter);
+                }
+
+                $valueToInterpolate = $this->filters[$filter]
+                    ->filter(
+                        $valueToInterpolate,
+                        $attribute
+                    );
+            }
+
+            $format = str_replace($matches[0][$key], $valueToInterpolate, $format);
+
+
+        }
+
+        return $format;
     }
 
+    public function addFilter(string $name, ValueFilterInterface $filter): void
+    {
+        $this->filters[$name] = $filter;
+    }
 }
