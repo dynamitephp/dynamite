c7463ba96057bb197618593b98cfc3a84a08d90e
pizzaminded <pizzaminded@users.noreply.github.com>
2022-03-28 22:36:15 +0200

    feat: KeyFormatResolver stub

---
+++ src/Dynamite/PrimaryKey/KeyFormatResolver.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dynamite\PrimaryKey;
+
+use Dynamite\Mapping\ItemMapping;
+
+class KeyFormatResolver
+{
+
+    public function resolve(string $format, ItemMapping $mapping, array $values): string
+    {
+
+    }
+
+}
