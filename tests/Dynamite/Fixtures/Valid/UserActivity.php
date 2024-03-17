b9546879266c5fd81d48056cb7955cd4922084a6
pizzaminded <pizzaminded@users.noreply.github.com>
2024-03-17 04:06:47 +0100

    fix: switch test classes from annotations to attributes

--- tests/Dynamite/Fixtures/Valid/UserActivity.php
+++ tests/Dynamite/Fixtures/Valid/UserActivity.php
@@ -5,35 +5,22 @@ namespace Dynamite\Fixtures\Valid;
 
 use Dynamite\Configuration as Dynamite;
 
-/**
- * @Dynamite\Item(objectType="USERACTIVITY")
- * @Dynamite\PartitionKeyFormat("USERACTIVITY#{userId}")
- * @Dynamite\SortKeyFormat("ACT#{activityId}")
- */
+#[Dynamite\Item(objectType:"USERACTIVITY")]
+#[Dynamite\PartitionKeyFormat("USERACTIVITY#{userId}")]
+#[Dynamite\SortKeyFormat("ACT#{activityId}")]
 class UserActivity
 {
 
-    /**
-     * @Dynamite\PartitionKey()
-     * @var string
-     */
+    #[Dynamite\PartitionKey()]
     private $pk;
 
-    /**
-     * @Dynamite\SortKey()
-     * @var string
-     */
+    #[Dynamite\SortKey()]
     private $sk;
 
-    /**
-     * @Dynamite\Attribute(type="string", name="userId")
-     * @var string
-     */
+    #[Dynamite\Attribute(type:"string", name:"userId")]
     private string $userId;
 
-    /**
-     * @Dynamite\Attribute(type="string", name="activityId")
-     * @var string
-     */
+
+    #[Dynamite\Attribute(type:"string", name:"activityId")]
     private string $activityId;
 }
